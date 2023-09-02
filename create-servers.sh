d4d6cbc5461f245a7537fd1ed6f37c5e0d3c3be7
SRK <RanjithSrikonda.Devops@example.com>
2023-09-02 17:07:14 +0530

    ansible-roboshop

--- create-servers.sh
+++ create-servers.sh
@@ -5,6 +5,7 @@ INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0728a62ab917d1728
 DOMAIN_NAME=devopsproject.cloud
+HOSTED_ZONE_ID=Z059197721KXEKXQH2LG
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -20,7 +21,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instances: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z059197721KXEKXQH2LG --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
